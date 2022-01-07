918b9d18c25f88c59551c7129704c33e0a13ce6d
heeeejus <mary2946@naver.com>
2022-01-07 20:40:33 +0900

    vue3-webpack

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ message }}</h1>
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from '~/componets/HelloWorld'
+
+export default {
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      message: 'Hello Vue!!'
+    }
+  }
+}
+</script>
